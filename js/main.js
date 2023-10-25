9b2b85185bfaa6a4225f1b6e354f93be24af086a
tOCHSKa <144660217+tOCHSKa@users.noreply.github.com>
2023-10-25 14:30:51 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
+// toggle navbar 
+
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+
+
+
+
+
+
 // scroll sections
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
@@ -14,7 +30,13 @@ window.onscroll = () => {
             navLinks.forEach(links => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-            })
+            });
+            // active sections for animation on scroll 
+            sec.classList.add('show-animate');
+        }
+        // if want to use animation that repeats on scroll use this
+
+        else { sec.classList.remove('show-animate');
         }
     });
 
@@ -22,4 +44,16 @@ window.onscroll = () => {
     let header = document.querySelector('header');
 
     header.classList.toggle('sticky', window.scrollY > 100);
+
+    // remove toggle icon and navbar when click navbar link scroll 
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+
+    // animation footer on scroll 
+
+    let footer = document.querySelector('footer');
+
+    footer.classList.toggle('show-animate',this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight);
+
 }
